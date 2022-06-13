829587eec1ce9a69d74c88a1d0a3bc173d3192ce
BirkhoffG <26811230+BirkhoffG@users.noreply.github.com>
2022-06-13 18:10:37 -0400

    update

--- label_script.py
+++ label_script.py
@@ -8,14 +8,14 @@ def link2id(x):
     return x.split('/')[-1].split('"')[0]
 
 def next_nan_label(df):
-    return df.index[df['Race'].isnull()][0]
+    return df.index[df['skin'].isnull()][0]
 
 def main(args):
-    img_pool_path = f"{args.img_pool}_label.csv"
+    img_pool_path = f"{args.img_pool}_skin.csv"
     img_dir = "img" if args.img_pool == 'fake' else 'original'
     img_pool_df = pd.read_csv(img_pool_path)
 
-    while img_pool_df['Race'].isnull().values.any():
+    while img_pool_df['skin'].isnull().values.any():
         idx = next_nan_label(img_pool_df)
         img_url = img_pool_df.loc[idx, 'INPUT:image']
         img_filename = link2id(img_url)
@@ -24,18 +24,17 @@ def main(args):
         img.show()
         text = input(f"""
 (id={idx}) Imge link: {img_url}
-    [0] white
-    [1] black
-    [2] asian
-    [3] indian
-    [4] others 
+    [0] Lighter (DEFAULT)
+    [1] Darker
 Label the race:
         """)
-        if text not in ['0', '1', '2', '3', '4']:
-            raise ValueError('Input must be an integer from 0 to 4.')
+        if not text:
+            text = '0'
+        if text not in ['0', '1']:
+            raise ValueError('Input must be an integer from 0 to 1.')
         
-        img_pool_df.loc[idx, 'Race'] = text
-        img_pool_df.to_csv(img_pool_path)
+        img_pool_df.loc[idx, 'skin'] = text
+        img_pool_df.to_csv(img_pool_path, index=None)
         print("result store")
 
         img.close()
@@ -43,7 +42,7 @@ Label the race:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img_pool', type=str, default='org', choices=['org', 'fake'])
+    parser.add_argument('--img_pool', type=str, default='fake', choices=['org', 'fake'])
     args = parser.parse_args()
     main(args)
 
